4328a2f86f13d66852dd49a40a76627be6de7588
rajgits14 <rajgits14@gmail.com>
2023-12-05 23:13:32 +0530

    Edit option logic

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDDToJymvrTC36xlOzX-43K9RRHSyvDh68",
-    authDomain: "firestore-test-b0931.firebaseapp.com",
+    apiKey: "-43K9RRHSyvDh68",
+    authDomain: "ebaseapp.com",
     projectId: "firestore-test-b0931",
     storageBucket: "firestore-test-b0931.appspot.com",
-    messagingSenderId: "323518381111",
-    appId: "1:323518381111:web:4d20fd60224289168913b2"
+    messagingSenderId: "",
+    appId: "1::web:4d20fd60224289168913b2"
   };
 
 firebase.initializeApp(firebaseConfig);
