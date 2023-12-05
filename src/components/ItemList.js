cb52e91b10261e0cb2a22395b3fc358e6e9aa1e1
rajgits14 <rajgits14@gmail.com>
2023-12-05 23:09:05 +0530

    Edit option logic

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -3,31 +3,60 @@ import { firestore } from '../firebase';
 
 export default function ItemList() {
     const [items, setItems] = useState([]);
+    const [editingItemId, setEditingItemId] = useState(null);
+    const [updatedItemName, setUpdatedItemName] = useState('');
 
     useEffect(() => {
         const unsubscribe = firestore.collection('items').onSnapshot((snapshot) => {
-          const newItems = snapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-          }));
-          setItems(newItems);
+            const newItems = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+            setItems(newItems);
         });
-    
+
         return () => unsubscribe();
-        }, []);
+    }, []);
 
-        const handleDelete = async (id) => {
-            await firestore.collection('items').doc(id).delete();
-        };
-  return (
-    <div>ItemList
+    const handleDelete = async (id) => {
+        await firestore.collection('items').doc(id).delete();
+    };
 
-    <ul>
-      {items.map((item) => (
-        <li key={item.id}>{item.name} <a href='#' onClick={() => handleDelete(item.id)}>Delete</a></li>
-      ))}
-    </ul>
-    
-    </div>
-  )
+    const handleEdit = (id, name) => {
+        setEditingItemId(id);
+        setUpdatedItemName(name);
+    };
+
+    const handleUpdate = async (id) => {
+        await firestore.collection('items').doc(id).update({ name: updatedItemName });
+        setEditingItemId(null);
+    };
+
+    return (
+        <div>
+            <p>ItemList</p>
+            <ul>
+                {items.map((item) => (
+                    <li key={item.id}>
+                        {editingItemId === item.id ? (
+                            <>
+                                <input
+                                    type="text"
+                                    value={updatedItemName}
+                                    onChange={(e) => setUpdatedItemName(e.target.value)}
+                                />
+                                <button onClick={() => handleUpdate(item.id)}>Update</button>
+                            </>
+                        ) : (
+                            <>
+                                {item.name}{' '}
+                                <button onClick={() => handleEdit(item.id, item.name)}>Edit</button>{' '}
+                                <button onClick={() => handleDelete(item.id)}>Delete</button>
+                            </>
+                        )}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
